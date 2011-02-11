1e28d1391e2e0223a00fa74d2a872a390f27b474
Pieter Colpaert <pieterc@member.fsf.org>
2011-02-12 00:24:31 +0100

    made a script which downloads all laws up to 1831 - check the scraper folder

--- scraper/fetcher.sh
+++ scraper/fetcher.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
-# This script will fetch all the html pages and stores them in a directory. As an input it wants the data returned by the scraper.pl script in this same directory.
+# This script will fetch all the html pages and stores them in a directory. It will try to search any date. If it finds laws it will increment the index. When an error is returned, we will go on to the next day
 #
 #
 # Disclaimer: You should not use this script. It is here for educational purpose only. It will generate a serious amount of traffic to fgov.be. You can download the entire datadump from our github page: github.com/iRail/BeLaws
@@ -11,10 +11,36 @@
 #
 # usage: bash fetcher.sh < results
 #
+y=2011
+m=2
+d=11
+n=1
 
-while read a ; do {
-  wget "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?caller=list&cn=$a&la=N&sql=dt+not+contains+'foo'&language=nl&chercher=t&fromtab=wet_all" -O laws/$a.html ;
+#as far as I know 1831 is the year Belgium was founded and no laws are made before that
+while [ $y -gt 1831 ] ; do {
+	#add a leading 0 if the number is less than 10
+	[ $n -lt 10 ] && ns=0$n;
+	[ $d -lt 10 ] && ds=0$d;
+	[ $m -lt 10 ] && ms=0$m;
+	
+	#with cn we're doing the request: concatenation of all parameters
+	cn=$y$ms$ds$ns;
+	
+#download the file and save it
+	wget "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?caller=list&cn=$cn&la=N&sql=dt+not+contains+'foo'&language=nl&chercher=t&fromtab=wet_all" -O laws/$cn.html ;
+	#if it does not contain a law, remove it, otherwise, increment te number
+	if grep "No article available" laws/$cn.html
+	then 
+	    rm laws/$cn.html;
+	    ((n=1));
+	    ((d--));
+	    [ $d -eq 0 ] && { d=31; ((m--)); }
+	    [ $m -eq 0 ] && { m=12; ((y--)); }
+	else 
+	    ((n++));
+	fi
+	
 } done
 
-#
+
 # cn is a lawID built like this: Y+M+D+n°ofLawThatDay
